64985a8a02d74f56fad7e9324612e929d8a3ef2c
Shawn Chestine <shawn@Shawns-MBP.askey.com>
2023-03-06 21:02:48 -0800

    Completes Objects lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+const employee = {
+    name: "John Doe",
+    streetAddress: "124 A St"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee};
+  
+    updatedEmployee[key] = value;
+  
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = employee;
+  
+    updatedEmployee[key] = value;
+  
+    return updatedEmployee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = {...employee};
+
+    delete updatedEmployee[key];
+
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const updatedEmployee = employee;
+
+    delete updatedEmployee[key];
+
+    return updatedEmployee;
+}
